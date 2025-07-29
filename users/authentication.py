f0de05756ac242ecde19d496d30793532fdac7e7
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-29 17:29:10 +0500

    create phone number autneticate

---
+++ users/authentication.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.backends import ModelBackend
+from .models import CustomUser
+
+
+class PhoneNumberBackend(ModelBackend):
+    def authenticate(self, request, phone_number=None, password=None, **kwargs):
+        try:
+            user = CustomUser.objects.get(phone_number=phone_number)
+            if user.check_password(password):
+                return user
+        except CustomUser.DoesNotExist:
+            return None
