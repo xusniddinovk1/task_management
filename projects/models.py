24b6083390258623af43184c5b10050c3729cc5c
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-29 17:04:22 +0500

    create project model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from users.models import CustomUser
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='owned_projects')
+    members = models.ManyToManyField(CustomUser, related_name='projects')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
