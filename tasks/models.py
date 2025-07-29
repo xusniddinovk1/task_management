2887b103c81e5ab443f1a6eb7ec4d7d0e2e95ce6
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-29 17:13:17 +0500

    update task model

--- tasks/models.py
+++ tasks/models.py
@@ -3,27 +3,26 @@ from projects.models import Project
 from users.models import CustomUser
 
 STATUS = (
-    ('TODO', 'todo'),
-    ('IN_PROGRESS', 'in_progress'),
-    ('REVIEW', 'review'),
-    ('DONE', 'done')
+    ('todo', 'To Do'),
+    ('in_progress', 'In Progress'),
+    ('review', 'In Review'),
+    ('done', 'Done')
 )
 
 PRIORITY_CHOICES = [
-    ('LOW', 'low'),
-    ('MEDIUM', 'medium'),
-    ('HIGH', 'high')
+    ('low', 'Low'),
+    ('medium', 'Medium'),
+    ('high', 'High')
 ]
 
-
 class Task(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks")
     title = models.CharField(max_length=100)
     description = models.TextField()
     assigned_to = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='tasks')
-    status = models.CharField(max_length=11, choices=STATUS, default='TODO')
-    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES, default='MEDIUM')
-    due_date = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=20, choices=STATUS, default='todo')
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='medium')
+    due_date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
