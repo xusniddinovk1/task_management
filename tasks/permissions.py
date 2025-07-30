dad6a1363bfdc09a70d84d2f3440d76e05afec6e
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-30 18:24:39 +0500

    create permissions

---
+++ tasks/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+
+
+class IsTaskAssigneeOrProjectMember(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        # Allows for admins
+        if request.user.is_staff:
+            return True
+        # Allow only for task member
+        if obj.assigned_to == request.user:
+            return True
+
+        if request.user in obj.project.members.all():
+            return True
+        return False
