77a1392085198415a661dd75a57093f41d80b102
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-31 11:17:30 +0500

    create viewset for project

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from tasks.permissions import IsProjectMember
+from rest_framework import viewsets
+from .models import Project
+from .serializers import ProjectSerializer
 
-# Create your views here.
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated, IsProjectMember]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Project.objects.all()
+        return Project.objects.filter(members=user)
