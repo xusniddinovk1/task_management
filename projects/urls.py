54be62d0ff085041a378af01acf834a7f6baa337
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-07-31 11:24:33 +0500

    update urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import *
+from .views import ProjectViewSet
 from tasks.views import TaskViewSet
 
 router = DefaultRouter()
+router.register(r'projects', ProjectViewSet)
 router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
