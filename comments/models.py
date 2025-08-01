dc12b34ff2a3e4efee34b3ba10b91bc7272384db
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-01 16:47:34 +0500

    create comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,13 @@
+from tasks.models import Task
+from users.models import CustomUser
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} comments to {self.task.id}'
