ae1ae4d4392cc7fc06261460364a2e1ab9f2c257
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-02 13:39:29 +0500

    add permission class

--- tasks/permissions.py
+++ tasks/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from tasks.models import Task
 
 
 class IsTaskAssigneeOrProjectMember(permissions.BasePermission):
@@ -17,7 +18,24 @@ class IsTaskAssigneeOrProjectMember(permissions.BasePermission):
 
 
 class IsProjectMember(permissions.BasePermission):
+
     def has_object_permission(self, request, view, obj):
         if request.user.is_staff:
             return True
         return request.user in obj.members.all()
+
+
+class IsTaskProjectMemberOrAdmin(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user.is_staff:
+            return True
+
+        if request.method == 'POST':
+            task_id = request.data.get('task')
+            try:
+                task = Task.objects.get(id=task_id)
+            except Task.DoesNotExist:
+                return False
+            return request.user in task.project.members.all()
+        return True
