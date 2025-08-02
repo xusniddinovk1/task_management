476abb695a1a3b59a0a83111cf1301c6e84ad3d2
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-02 09:21:15 +0500

    update views

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Comment
+from .serializers import CommentSerializer
+from tasks.permissions import IsTaskAssigneeOrProjectMember
 
-# Create your views here.
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsTaskAssigneeOrProjectMember]
