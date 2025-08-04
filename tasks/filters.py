6e8d56074b14be708d5fe4167df0ca3b92ef0932
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-04 14:13:39 +0500

    update filters

--- tasks/filters.py
+++ tasks/filters.py
@@ -3,11 +3,11 @@ from .models import Task
 
 
 class TaskFilter(django_filters.FilterSet):
+    status = django_filters.CharFilter(field_name='status', lookup_expr='exact')
+    priority = django_filters.CharFilter(field_name='priority', lookup_expr='exact')
+    project = django_filters.NumberFilter(field_name='project__id', lookup_expr='exact')
+    assigned_to = django_filters.NumberFilter(field_name='assigned_to__id', lookup_expr='exact')
+
     class Meta:
         model = Task
-        fields = {
-            'status': ['exact'],
-            'priority': ['exact'],
-            'project': ['exact'],
-            'assigned_to': ['exact'],
-        }
+        fields = ['status', 'priority', 'project', 'assigned_to']
--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,23 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from .filters import TaskFilter
 from .permissions import IsTaskAssigneeOrProjectMember
 from .serializers import TaskSerializer
 from .models import Task
+from rest_framework import filters
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated, IsTaskAssigneeOrProjectMember]
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_class = TaskFilter
+    search_fields = ['title', 'description']
 
     def get_queryset(self):
         user = self.request.user
